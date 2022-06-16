ea4c61588610e2104a4f56e3d79a0f7006b6bcf2
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-06-16 13:42:17 +0300

    Add project files.

---
+++ task/CFraction.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+class CFraction
+{
+private:
+	int whole; // целое
+	int numerator; // числитель
+	int denominator; // знаменатель
+public:
+	int getWhole() { return whole; }
+	int getNumerator() { return numerator; }
+	int getDenominator() { return denominator; }
+	void setWhole(int whl) { whole = whl; }
+	void setNumerator(int nmrtr) { numerator = nmrtr; }
+	void setDenominator(int dnmntr) { denominator = dnmntr; }
+
+	// конструктор по умолчанию
+	CFraction()
+	{
+		whole = 0;
+		numerator = 1;
+		denominator = 2;
+	};
+	// конструктор без целой части
+	CFraction(int nmrtr, int dnmntr)
+	{
+		whole = 0;
+		numerator = nmrtr;
+		denominator = dnmntr;
+	};
+	// конструктор с целой частью
+	CFraction(int whl, int nmrtr, int dnmntr)
+	{
+		whole = whl;
+		numerator = nmrtr;
+		denominator = dnmntr;
+	};
+};
+
+// Функция вычисляет наибольший общий делитель целых чисел (сокращенно НОД)
+int GCD(int a, int b);
+
+// Функция вычисляет наименьшее общее кратное (сокращенно НОК)
+int LCM(int a, int b);
+
+// Функция сокращения дробей
+CFraction FractReduction(CFraction f);
+
+// Функция превращения неправильной дроби в смешанное число.
+CFraction ConvIncorFractToMixNum(CFraction f);
+
+// Функция превращения смешанного числа в неправильную дробь.
+CFraction ConvMixedNumToIncorrFract(CFraction f);
+
+// Функция реализующая операцию сложения
+CFraction AdditionOfFractions(CFraction f1, CFraction f2);
+
+// Функция для ввода дроби
+void Input(CFraction& f);
+
+// Функция для вывода дроби
+void Print(CFraction f);
+
+// Функция выполняет вычитание дробей
+CFraction FractionSubtraction(CFraction f1, CFraction f2);
+
+// Функция реализующая операцию умножения
+CFraction Multiplication(CFraction f1, CFraction f2);
+
+// Функция реализующая операцию деления
+CFraction Division(CFraction f1, CFraction f2);
---
+++ task/task.cpp
@@ -0,0 +1,70 @@
+﻿/*
+	Для разработанного ранее класса CFraction
+перегрузить арифметические операции сложения
+вычитания, умножения и деления, а также операции
+отношения. Предусмотреть два способа перегрузки:
+	• методами класса;
+	• глобальными функциями.
+
+	СТАТУС : Не готово.
+	ВОЗМОЖНЫЕ ДОРАБОТКИ:
+*
+	ИЗВЕСТНЫЕ ОШИБКИ:
+*
+	ВИДЕО: 03:07:55(начало дз)
+*/
+
+#include <iostream>
+#include "CFraction.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	CFraction f1{}, f2{}, result{};
+	char action; // действие с дробями
+
+	char answer{ 0 }; // Do you want to continue?
+
+	do
+	{
+		cout << "Enter first fraction.\n";
+		Input(f1);
+
+		cout << "Enter an action: ";
+		cin >> action;
+
+		cout << "Enter second fraction.\n";
+		Input(f2);
+
+		switch (action)
+		{
+		case '+':
+			result = AdditionOfFractions(f1, f2);
+			break;
+
+		case '-':
+			result = FractionSubtraction(f1, f2);
+			break;
+
+		case '*':
+			result = Multiplication(f1, f2);
+			break;
+
+		case '/':
+			result = Division(f1, f2);
+			break;
+		}
+
+		Print(result);
+
+		cout << endl;
+		cout << "Do you want to continue? ( y (yes) / n (no) )\n";
+		cin >> answer;
+		cout << endl;
+
+	} while (answer == 'y');
+
+	return 0;
+}
